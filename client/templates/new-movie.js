3dd7a06e98cee01e584f73ab7488b04c44149584
thechori <thechori@gmail.com>
2016-04-08 18:30:04 -0500

    Adds ability to create new movies via the newMovie page

---
+++ client/templates/new-movie.js
@@ -0,0 +1,36 @@
+if (Meteor.isClient) {
+  console.log("isClient");
+
+  Template.newMovie.events({
+    // Works properly
+    'submit form': function(event) {
+      event.preventDefault();
+
+      console.log("submitting form from Template.newMovie.events")
+
+      // Grab values from the form
+      let title = event.target.title.value;
+      let date = event.target.date.value;
+
+      // Submit the movie to the movies collection
+      Movies.insert({
+        dateCreated: new Date(),
+        title,
+        watchedOn: date
+      });
+
+      // Clear the form
+      event.target.title.value = "";
+      event.target.date.value = "";
+    }
+  });
+
+  Template.newMovie.helpers({
+    handleSubmit: function(event) {
+      event.preventDefault();
+      console.log("submitting the form!");
+      console.log(event);
+    }
+  });
+
+}
